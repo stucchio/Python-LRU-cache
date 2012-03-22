e0b9a1867070487b3ff731b0e43e95cd4c0d6eb4
Chris Stucchio <stucchio@gmail.com>
2012-03-22 10:31:43 -0400

    Cleaned up lru cache, added doctests.

--- lru.py
+++ lru.py
@@ -2,81 +2,173 @@ from collections import OrderedDict
 from time import time
 from itertools import islice
 
-class LRUCachedFunction(object):
-    def __init__(self, function, max_size=1024, expiration=15*60):
+def lru_cache_function(max_size=1024, expiration=15*60):
+    def wrapper(func):
+        return LRUCachedFunction(func, LRUCacheDict(max_size, expiration))
+    return wrapper
+
+
+class LRUCacheDict(object):
+    """ A dictionary-like object, supporting LRU caching semantics.
+
+    >>> d = LRUCacheDict(max_size=3, expiration=3)
+    >>> d['foo'] = 'bar'
+    >>> d['foo']
+    'bar'
+    >>> import time
+    >>> time.sleep(4) # 4 seconds > 3 second cache expiry of d
+    >>> d['foo']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'foo'
+    >>> d['a'] = 'A'
+    >>> d['b'] = 'B'
+    >>> d['c'] = 'C'
+    >>> d['d'] = 'D'
+    >>> d['a'] # Should return value error, since we exceeded the max cache size
+    Traceback (most recent call last):
+        ...
+    KeyError: 'a'
+    """
+    def __init__(self, max_size=1024, expiration=15*60):
         self.max_size = max_size
         self.expiration = expiration
-        self.function = function
 
         self.__values = {}
         self.__expire_times = OrderedDict()
         self.__access_times = OrderedDict()
-        self.__name__ = function.__name__
 
     def size(self):
         return len(self.__values)
 
     def clear(self):
-        for k in self.__values.iterkeys():
-            self.__del_item(k)
+        """
+        Clears the dict.
 
-    def __call__(self, *args, **kwargs):
-        self.__cleanup()
-        key = repr( (args, kwargs) )
+        >>> d = LRUCacheDict(max_size=3, expiration=1)
+        >>> d['foo'] = 'bar'
+        >>> d['foo']
+        'bar'
+        >>> d.clear()
+        >>> d['foo']
+        Traceback (most recent call last):
+        ...
+        KeyError: 'foo'
+        """
+        self.__values.clear()
+        self.__expire_times.clear()
+        self.__access_times.clear()
 
+    def has_key(self, key):
+        """
+        This method should almost NEVER be used. The reason is that between the time
+        has_key is called, and the key is accessed, the key might vanish.
+
+        You should ALWAYS use a try: ... except KeyError: ... block.
+
+        >>> d = LRUCacheDict(max_size=3, expiration=1)
+        >>> d['foo'] = 'bar'
+        >>> d['foo']
+        'bar'
+        >>> import time
+        >>> if d.has_key('foo'):
+        ...    time.sleep(2) #Oops, the key 'foo' is gone!
+        ...    d['foo']
+        Traceback (most recent call last):
+        ...
+        KeyError: 'foo'
+        """
+        return self.__values.has_key(key)
+
+    def __setitem__(self, key, value):
         t = int(time())
+        self.__delete__(key)
+        self.__values[key] = value
+        self.__access_times[key] = t
+        self.__expire_times[key] = t + self.expiration
+        self.cleanup()
 
-        if self.__values.has_key(key):
-            del self.__access_times[key]
-            self.__access_times[key] = t
-            return self.__values[key]
-        else:
-            self.__values[key] = self.function(*args, **kwargs)
-            self.__access_times[key] = t
-            self.__expire_times[key] = t + self.expiration
+    def __getitem__(self, key):
+        t = int(time())
+        del self.__access_times[key]
+        self.__access_times[key] = t
+        self.cleanup()
+        return self.__values[key]
 
-
-    def __del_item(self, key):
+    def __delete__(self, key):
         if self.__values.has_key(key):
             del self.__values[key]
             del self.__expire_times[key]
             del self.__access_times[key]
 
-    def __cleanup(self):
+    def cleanup(self):
         if self.expiration is None:
             return None
         t = int(time())
         #Delete expired
         for k in self.__expire_times.iterkeys():
             if self.__expire_times[k] < t:
-                self.__del_item(k)
+                self.__delete__(k)
             else:
                 break
 
         #If we have more than self.max_size items, delete the oldest
-        while (len(self.__values) > self.max_size - 1):
+        while (len(self.__values) > self.max_size):
             for k in self.__access_times.iterkeys():
-                self.__del_item(k)
+                self.__delete__(k)
                 break
 
+class LRUCachedFunction(object):
+    """
+    A memoized function, backed by an LRU cache.
 
+    >>> def f(x):
+    ...    print "Calling f(" + str(x) + ")"
+    ...    return x
+    >>> f = LRUCachedFunction(f, LRUCacheDict(max_size=3, expiration=3) )
+    >>> f(3)
+    Calling f(3)
+    3
+    >>> f(3)
+    3
+    >>> import time
+    >>> time.sleep(4) #Cache should now be empty, since expiration time is 3.
+    >>> f(3)
+    Calling f(3)
+    3
+    >>> f(4)
+    Calling f(4)
+    4
+    >>> f(5)
+    Calling f(5)
+    5
+    >>> f(3) #Still in cache, so no print statement. At this point, 4 is the least recently used.
+    3
+    >>> f(6)
+    Calling f(6)
+    6
+    >>> f(4) #No longer in cache - 4 is the least recently used, and there are at least 3 others items in cache [3,4,5,6].
+    Calling f(4)
+    4
 
-if __name__=="__main__":
-    from time import sleep
+    """
+    def __init__(self, function, cache=None):
+        if cache:
+            self.cache = cache
+        else:
+            self.cache = LRUCacheDict()
+        self.function = function
+        self.__name__ = self.function.__name__
 
-    def test(i):
-        print 'Calling test with ' + str(i)
-        return str(i) + " - result"
-    f = LRUCachedFunction(test, 5, 3)
-    for i in range(7):
-        f(i)
-    for i in range(3,7): #This should result in print nothing, since 3-6 are already in the queue
-        f(i)
-    f(0) #This should result in a print statement, since the 0'th element was removed from the cache due to size issues
-    sleep(5)
-    print "Continuing"
-    for i in range(4): #Cache should be empty, so this should result in prints
-        f(i)
-    for i in range(4): #But this should not result in anything being printed
-        f(i)
+    def __call__(self, *args, **kwargs):
+        key = repr( (args, kwargs) )
+        try:
+            return self.cache[key]
+        except KeyError:
+            value = self.function(*args, **kwargs)
+            self.cache[key] = value
+            return value
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
