ab8151376375b93f94502465229bafd833e0531b
Chris Stucchio <stucchio@gmail.com>
2014-06-21 16:20:44 +0530

    ready to publish, I think

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name='lru_cache',
+      version='0.1',
+      description='LRU cache',
+      author='Chris Stucchio',
+      author_email='stucchio@gmail.com',
+      url='https://github.com/stucchio/Python-LRU-cache',
+      packages = ['lru'],
+     )
